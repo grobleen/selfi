03b66ee6f1be83c5e5e64c984cb0a1df827692f3
grobleen <111244776+grobleen@users.noreply.github.com>
2023-03-26 13:25:32 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -7,14 +7,21 @@ recognition.start();
 recognition.onresult = function(event){
 var contenido = event.results[0][0].transcript;    
 document.getElementById("textbox").innerHTML=contenido;
+if(contenido == "tomar"){
+console.log ("tomandofoto");
 speak();
+}
 }
 function speak(){
 var synth = window.speechSynthesis;
-speak_data = document.getElementById("textbox").value
+speak_data = "tomandofotoencincosegundos";
 var utterThis = new SpeechSynthesisUtterance(speak_data);
 synth.speak(utterThis);
 Webcam.attach(camara);
+setTimeout(function(){
+tomar_foto();
+guardar();
+},5000);
 }
 camara = document.getElementById("camara");
 Webcam.set({
@@ -23,3 +30,14 @@ height:250,
 image_format:'png',
 png_quality : 90   
 });
+function tomar_foto(){
+Webcam.snap(function(data_uri){
+document.getElementById("resultado").innerHTML=  '<img id="selfie_image" src="'+data_uri+'"/>';  
+});
+}
+function guardar(){
+var link = document.getElementById ("link");
+var imagen = document.getElementById ("selfie_image").src;
+link.href=imagen;
+link.click();   
+}
